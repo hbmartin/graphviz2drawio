63c539d6bccfccba90e8d875eba43df1be30e90d
Jonah Caplan <jcaplan@blackberry.com>
2018-09-05 19:37:32 -0400

    add black check to build

--- test/test_graphs.py
+++ test/test_graphs.py
@@ -8,43 +8,43 @@ num_cells_offset = 2
 
 
 def check_xml_top(root):
-    graph = root.findall('.')
+    graph = root.findall(".")
     assert len(graph) == 1
     graph = graph[0]
-    assert graph.tag == 'mxGraphModel'
-    return graph.findall('./root/*')
+    assert graph.tag == "mxGraphModel"
+    return graph.findall("./root/*")
 
 
 def check_style(e, check):
-    assert check in e.attrib['style']
+    assert check in e.attrib["style"]
 
 
 def check_value(e, check):
-    value = e.attrib['value']
-    match = re.match(r'<p.*>%s</p>' % check, html.unescape(value))
-    assert match, 'no match found for %s' % value
+    value = e.attrib["value"]
+    match = re.match(r"<p.*>%s</p>" % check, html.unescape(value))
+    assert match, "no match found for %s" % value
 
 
 def check_edge(e, source, target):
-    assert e.attrib.get('edge')
-    assert e.attrib['source'] == source.attrib['id']
-    assert e.attrib['target'] == target.attrib['id']
+    assert e.attrib.get("edge")
+    assert e.attrib["source"] == source.attrib["id"]
+    assert e.attrib["target"] == target.attrib["id"]
 
 
 def check_edge_dir(e, dx, dy):
-    style = [e.split('=') for e in e.attrib['style'].split(';')][:-1]
+    style = [e.split("=") for e in e.attrib["style"].split(";")][:-1]
     style = {e[0]: e[1] for e in style if e}
-    x2 = float(style['exitX'])
-    x1 = float(style['entryX'])
-    y2 = float(style['exitY'])
-    y1 = float(style['entryY'])
+    x2 = float(style["exitX"])
+    x1 = float(style["entryX"])
+    y2 = float(style["exitY"])
+    y1 = float(style["entryY"])
 
     assert (x2 - x1) == dx
     assert (y2 - y1) == dy
 
 
 def test_hello():
-    file = './directed/hello.gv.txt'
+    file = "./directed/hello.gv.txt"
     xml = graphviz2drawio.convert(file)
     print(xml)
 
@@ -52,12 +52,12 @@ def test_hello():
     elements = check_xml_top(root)
 
     hello = elements[2]
-    check_style(hello, 'ellipse')
-    check_value(hello, 'Hello')
+    check_style(hello, "ellipse")
+    check_value(hello, "Hello")
 
     world = elements[3]
-    check_style(world, 'ellipse')
-    check_value(world, 'World')
+    check_style(world, "ellipse")
+    check_value(world, "World")
     edge = elements[4]
     check_edge(edge, hello, world)
     check_edge_dir(edge, dx=0, dy=1)
