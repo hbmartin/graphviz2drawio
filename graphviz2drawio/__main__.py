fd8560e316b5656d5044da3d408c66ee10603489
Harold Martin <harold.martin@gmail.com>
2018-09-08 08:04:05 -0700

    make crash reporting optional

--- graphviz2drawio/__main__.py
+++ graphviz2drawio/__main__.py
@@ -5,16 +5,6 @@ from .version import __version__
 
 
 def main():
-    client = None
-    try:
-        from raven import Client
-
-        client = Client(
-            dsn="https://f1fce21b51864819a26ea116ff4e5b7f:9f61501e4891465ea53e0416e5f402b1@sentry.io/1266157",
-            release=__version__,
-        )
-    except BaseException:
-        pass
 
     args = Arguments(__version__).parse_args()
 
@@ -26,7 +16,14 @@ def main():
         output = convert(args.to_convert, args.program)
     except BaseException:
         stderr.write("Something went wrong, please report\n")
-        if client is not None:
+        stderr.write("An automatic crash report can be sent to the developer (no personal or graph information)\n")
+        permission = input("Type 'no' to cancel report, press enter to send: ")
+        if permission != "no":
+            from raven import Client
+            client = Client(
+                dsn="https://f1fce21b51864819a26ea116ff4e5b7f:9f61501e4891465ea53e0416e5f402b1@sentry.io/1266157",
+                release=__version__,
+            )
             client.captureException()
         raise
 
