0022091d11862e25955074595908a95f6798aef2
Rafael Leira <rafael.leira@naudit.es>
2020-01-20 15:14:12 +0100

    minor fix for some dot versions

--- graphviz2drawio/mx/CurveFactory.py
+++ graphviz2drawio/mx/CurveFactory.py
@@ -1,4 +1,4 @@
-from svg.path import CubicBezier
+from svg.path import CubicBezier, Move
 from svg.path import parse_path
 from .Curve import Curve
 
@@ -11,17 +11,19 @@ class CurveFactory:
     def from_svg(self, svg_path):
         path = parse_path(svg_path)
         start = self.coords.complex_translate(path[0].start)
-        end = self.coords.complex_translate(path[1].end)
+        end = self.coords.complex_translate(path[len(path) - 1].end)
         cb = None
         cbset = []
-        if isinstance(path[1], CubicBezier):
+        if isinstance(path[0], Move):
+            path = [path[i] for i in range(1, len(path))]
+        if isinstance(path[0], CubicBezier):
             # TODO: needs to account for multiple bezier in path
-            points = [path[1].start, path[1].control1, path[1].control2, path[1].end]
+            points = [path[0].start, path[0].control1, path[0].control2, path[0].end]
             if not Curve.is_linear(points):
                 cb = [self.coords.complex_translate(p) for p in points]
 
             if len(path) > 2:  # set of curves/points
-                for r in range(1, len(path)):
+                for r in range(0, len(path)):
                     cbset.append(
                         (
                             self.coords.translate(
