43b50d15754f22438ed3e1bacb3d5b36ecdbf871
naudit <sondaalcala@naudit.es>
2020-01-20 11:18:41 +0100

    Naive aproximation to multiple-point lines (not full-bezier support)

--- graphviz2drawio/mx/CurveFactory.py
+++ graphviz2drawio/mx/CurveFactory.py
@@ -13,9 +13,22 @@ class CurveFactory:
         start = self.coords.complex_translate(path[0].start)
         end = self.coords.complex_translate(path[1].end)
         cb = None
+        cbset = []
         if isinstance(path[1], CubicBezier):
             # TODO: needs to account for multiple bezier in path
-            points = [path[1].start, path[1].control1, path[1].control2, path[1].end]
+            points = [path[1].start, path[1].control1,
+                      path[1].control2, path[1].end]
             if not Curve.is_linear(points):
                 cb = [self.coords.complex_translate(p) for p in points]
-        return Curve(start=start, end=end, cb=cb)
+
+            if len(path) > 2:  # set of curves/points
+                for r in range(1, len(path)):
+                    cbset.append(
+                        (
+                            self.coords.translate(
+                                path[r].start.real, path[r].start.imag),
+                            self.coords.translate(
+                                path[r].end.real, path[r].end.imag)
+                        )
+                    )
+        return Curve(start=start, end=end, cb=cb, cbset=cbset)
