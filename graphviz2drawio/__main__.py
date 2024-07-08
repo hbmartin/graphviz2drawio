7598868c2057d18f0ff98ef14c4dbf9d2dd9a2bf
Harold Martin <harold.martin@gmail.com>
2024-07-08 10:00:53 -0700

    Remove sentry, support multiple file conversion

--- graphviz2drawio/__main__.py
+++ graphviz2drawio/__main__.py
@@ -1,48 +1,56 @@
 import pathlib
+import sys
 from sys import stderr
 
 from .graphviz2drawio import convert
 from .models import Arguments
 from .version import __version__
 
+DEFAULT_TEXT = "\033[0m"
+RED_TEXT = "\033[91m"
+BOLD = "\033[1m"
 
-def main() -> None:
 
-    args = Arguments(__version__).parse_args()  # pytype: disable=not-callable
+def _gv_filename_to_xml(filename: str) -> str:
+    return ".".join(filename.split(".")[:-1]) + ".xml"
 
-    if not args.stdout:
-        stderr.write("This is beta software, please report issues to:\n")
-        stderr.write("https://github.com/hbmartin/graphviz2drawio/issues\n")
 
+def _convert_file(to_convert: str, program: str, outfile: str | None) -> None:
     try:
-        output = convert(args.to_convert, args.program)
+        output = convert(to_convert, program)
     except BaseException:
-        stderr.write("Something went wrong, please file an issue.\n")
-        stderr.write("This tool can automatically report using sentry.io.\n")
-        stderr.write("The report contains no PII or graph data (only the file path).\n")
-        permission = input("Type 'no' to cancel report, press enter to send: ")
-        if permission != "no":
-            from raven import Client
-
-            client = Client(
-                dsn="https://f1fce21b51864819a26ea116ff4e5b7f:9f61501e4891465ea53e0416e5f402b1@sentry.io/1266157",
-                release=__version__,
-            )
-            client.captureException()
+        stderr.write(f"{RED_TEXT}{BOLD}Error converting {to_convert}\n")
+        stderr.write("https://github.com/hbmartin/graphviz2drawio/issues\n")
+        stderr.write("Please include an example of your diagram and the following:\n\n")
+        stderr.write(DEFAULT_TEXT)
+        stderr.write(f"Python: {sys.version}, g2d: {__version__}\n")
         raise
 
-    if args.stdout:
+    if outfile is None:
         print(output)
         return
 
-    if args.outfile is not None:
-        outfile = args.outfile
-    else:
-        base = args.to_convert.split(".")
-        outfile = ".".join(base[:-1]) + ".xml"
-
     pathlib.Path(outfile).write_text(output)
     stderr.write("Converted file: " + outfile + "\n")
+
+
+def main() -> None:
+    args = Arguments(__version__).parse_args()  # pytype: disable=not-callable
+
+    if args.outfile is None or len(args.outfile) == 0:
+        in_files = [args.to_convert]
+        out_files = [_gv_filename_to_xml(args.to_convert) if not args.stdout else None]
+    elif len(args.outfile) == 1:
+        in_files = [args.to_convert]
+        out_files = args.outfile
+    else:
+        in_files = [args.to_convert, *args.outfile]
+        out_files = [_gv_filename_to_xml(args.to_convert)] + [
+            _gv_filename_to_xml(f) for f in args.outfile
+        ]
+
+    for in_file, out_file in zip(in_files, out_files, strict=True):
+        _convert_file(in_file, args.program, out_file)
 
 
 if __name__ == "__main__":
--- graphviz2drawio/models/Arguments.py
+++ graphviz2drawio/models/Arguments.py
@@ -3,11 +3,28 @@ from argparse import ArgumentParser
 
 class Arguments(ArgumentParser):
     def __init__(self, version: str) -> None:
-        super().__init__(prog="graphviz2drawio")
-        self.add_argument("to_convert", metavar="file.dot", help="graphviz file")
-        output = self.add_mutually_exclusive_group()
-        output.add_argument("outfile", help="output file", nargs="?", default=None)
-        output.add_argument("--stdout", action="store_true", help="print xml to stdout")
+        super().__init__(
+            prog="graphviz2drawio",
+            description="Please report issues at: https://github.com/hbmartin/graphviz2drawio/issues",
+        )
+        self.add_argument(
+            "to_convert",
+            metavar="file.dot",
+            help="Path of the graphviz file to convert.",
+        )
+        self.add_argument(
+            "outfile",
+            metavar="outfile.xml",
+            help="Optional path to output XML. "
+            "If more than one file is given, they will be treated as input files.",
+            nargs="*",
+            default=None,
+        )
+        self.add_argument(
+            "--stdout",
+            action="store_true",
+            help="Print converted XML to stdout instead of writing a file.",
+        )
         self.add_argument(
             "--version",
             action="version",
