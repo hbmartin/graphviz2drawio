002cf4fd768acdf6a8c1341ee136e8c9907a2ddd
Harold Martin <harold.martin@gmail.com>
2024-08-12 17:01:27 -0700

    Support for stdin, arg parsing cleanup

--- graphviz2drawio/__main__.py
+++ graphviz2drawio/__main__.py
@@ -1,5 +1,6 @@
 import pathlib
 import sys
+from io import TextIOWrapper
 from sys import stderr
 
 from .graphviz2drawio import convert
@@ -15,16 +16,19 @@ def _gv_filename_to_xml(filename: str) -> str:
     return ".".join(filename.split(".")[:-1]) + ".xml"
 
 
-def _convert_file(to_convert: str, program: str, outfile: str | None) -> None:
+def _convert_file(to_convert: TextIOWrapper, program: str, outfile: str | None) -> None:
     try:
-        output = convert(to_convert, program)
+        output = convert(to_convert.read(), program)
     except BaseException:
         stderr.write(f"{RED_TEXT}{BOLD}Error converting {to_convert}\n")
+        stderr.write("Please open a report at\n")
         stderr.write("https://github.com/hbmartin/graphviz2drawio/issues\n")
-        stderr.write("Please include an example of your diagram and the following:\n\n")
+        stderr.write("and include your diagram and the following error:\n\n")
         stderr.write(DEFAULT_TEXT)
         stderr.write(f"Python: {sys.version}, g2d: {__version__}\n")
         raise
+    finally:
+        to_convert.close()
 
     if outfile is None:
         print(output)
@@ -38,25 +42,41 @@ def main() -> None:
     args = Arguments(__version__).parse_args()  # pytype: disable=not-callable
 
     in_files: list[str]
-    out_files: list[str] | None
-    if args.outfile is None or len(args.outfile) == 0:
-        in_files = [args.to_convert]
-        out_files = None if args.stdout else [_gv_filename_to_xml(args.to_convert)]
-    elif len(args.outfile) == 1:
-        in_files = [args.to_convert]
-        out_files = args.outfile
-    else:
-        in_files = [args.to_convert, *args.outfile]
-        out_files = [
-            _gv_filename_to_xml(args.to_convert),
-            *[_gv_filename_to_xml(f) for f in args.outfile],
-        ]
+    out_files: list[str | None]
 
-    if out_files is None:
-        _convert_file(in_files[0], args.program, None)
+    if len(args.to_convert) > 1 and args.stdout:
+        print("Only one file can be converted when using --stdout")
+        sys.exit(1)
+    if len(args.to_convert) > 1 and args.outfile is not None:
+        print("Only one file can be converted when specifying an output file")
+        sys.exit(1)
+    if len(args.to_convert) == 0 or (
+        len(args.to_convert) == 1
+        and args.to_convert[0] == sys.stdin
+        and sys.stdin.isatty()
+    ):
+        Arguments(__version__).print_help()
+        sys.exit(1)
+
+    if args.stdout and args.outfile is not None:
+        sys.stdout.write(f"Writing to {args.outfile} (ignoring stdout)\n")
+
+    if len(args.to_convert) == 1:
+        in_files = args.to_convert
+        if args.to_convert[0] == sys.stdin:
+            out_files = [args.outfile] if args.outfile is not None else [None]
+        elif args.outfile is not None:
+            out_files = [args.outfile]
+        elif args.stdout:
+            out_files = [None]
+        else:
+            out_files = [_gv_filename_to_xml(args.to_convert[0].name)]
     else:
-        for in_file, out_file in zip(in_files, out_files, strict=True):
-            _convert_file(in_file, args.program, out_file)
+        in_files = args.to_convert
+        out_files = [_gv_filename_to_xml(in_file.name) for in_file in args.to_convert]
+
+    for in_file, out_file in zip(in_files, out_files, strict=True):
+        _convert_file(in_file, args.program, out_file)
 
 
 if __name__ == "__main__":
