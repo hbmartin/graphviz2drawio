aba159cf6716d8ec8c1e0f7082a340472be26ece
Harold Martin <harold.martin@gmail.com>
2024-08-14 10:41:35 -0700

    add permissions to workflows, reformat Styles

--- graphviz2drawio/mx/Styles.py
+++ graphviz2drawio/mx/Styles.py
@@ -5,14 +5,26 @@ from . import Shape
 
 # Make this subclass StrEnum when dropping Py 3.10 support
 class Styles(Enum):
-    NODE = "verticalAlign={vertical_align};html=1;rounded=0;labelBackgroundColor=none;strokeColor={stroke};strokeWidth=1;fillColor={fill};"
-    EDGE = "html=1;endArrow={end_arrow};dashed={dashed};endFill={end_fill};startArrow={start_arrow};startFill={start_fill};fillColor={stroke};strokeColor={stroke};"
+    NODE = (
+        "verticalAlign={vertical_align};html=1;rounded=0;labelBackgroundColor=none;strokeColor={stroke};"
+        "strokeWidth=1;fillColor={fill};"
+    )
+    EDGE = (
+        "html=1;endArrow={end_arrow};dashed={dashed};endFill={end_fill};startArrow={start_arrow};"
+        "startFill={start_fill};fillColor={stroke};strokeColor={stroke};"
+    )
     EDGE_LABEL = (
         "edgeLabel;html=1;align=center;verticalAlign=bottom;resizable=0;points=[];"
     )
-    EDGE_INVIS = "rounded=1;html=1;exitX={exit_x:.3g};exitY={exit_y:.3g};jettySize=auto;curved={curved};endArrow={end_arrow};dashed={dashed};endFill={end_fill};"
+    EDGE_INVIS = (
+        "rounded=1;html=1;exitX={exit_x:.3g};exitY={exit_y:.3g};jettySize=auto;curved={curved};"
+        "endArrow={end_arrow};dashed={dashed};endFill={end_fill};"
+    )
 
-    TEXT_VALUE = "{open_tags}<font style='font-size: {size}px;' face='{family}' color='{color}'>{text}</font>{close_tags}"
+    TEXT_VALUE = (
+        "{open_tags}<font style='font-size: {size}px;' face='{family}' color='{color}'>"
+        "{text}</font>{close_tags}"
+    )
 
     ELLIPSE = "ellipse;" + NODE
     CIRCLE = "ellipse;aspect=fixed;" + NODE
@@ -31,7 +43,8 @@ class Styles(Enum):
     OCTAGON = "shape=mxgraph.basic.octagon2;align=center;dx=15;" + NODE
     DOUBLE_CIRCLE = "ellipse;shape=doubleEllipse;aspect=fixed;" + NODE
     DOUBLE_OCTAGON = (
-        "shape=image;html=1;verticalLabelPosition=middle;imageAspect=0;aspect=fixed;image=https://cdn4.iconfinder.com/data/icons/feather/24/octagon-128.png;labelPosition=center;align=center;"
+        "shape=image;html=1;verticalLabelPosition=middle;imageAspect=0;aspect=fixed;"
+        "image=https://cdn4.iconfinder.com/data/icons/feather/24/octagon-128.png;labelPosition=center;align=center;"
         + NODE
     )
     INV_TRIANGLE = "triangle;direction=south;" + NODE
@@ -44,13 +57,16 @@ class Styles(Enum):
         "shape=mxgraph.basic.star;labelPosition=center;align=center;verticalLabelPosition=middle;"
         + NODE
     )
-    UNDERLINE = "line;strokeWidth=2;verticalAlign=bottom;labelPosition=center;verticalLabelPosition=top;align=center;"
+    UNDERLINE = (
+        "line;strokeWidth=2;verticalAlign=bottom;labelPosition=center;"
+        "verticalLabelPosition=top;align=center;"
+    )
     CYLINDER = "shape=cylinder;boundedLbl=1;backgroundOutline=1;" + NODE
     NOTE = "shape=note;backgroundOutline=1;" + NODE
     TAB = "shape=folder;tabWidth=40;tabHeight=14;tabPosition=left;" + NODE
     FOLDER = (
-        "shape=mxgraph.office.concepts.folder;outlineConnect=0;align=center;verticalLabelPosition=middle;labelPosition=center;shadow=0;dashed=0;"
-        + NODE
+        "shape=mxgraph.office.concepts.folder;outlineConnect=0;align=center;verticalLabelPosition=middle;"
+        "labelPosition=center;shadow=0;dashed=0;" + NODE
     )
     CUBE = "shape=cube;boundedLbl=1;backgroundOutline=1;" + NODE
     COMPONENT = "shape=component;align=center;spacingLeft=36;" + NODE
@@ -64,13 +80,13 @@ class Styles(Enum):
         + NODE
     )
     RARROW = (
-        "shape=mxgraph.arrows2.arrow;dy=0.6;dx=40;align=center;labelPosition=center;notch=0;strokeWidth=2;verticalLabelPosition=middle;"
-        + NODE
+        "shape=mxgraph.arrows2.arrow;dy=0.6;dx=40;align=center;labelPosition=center;notch=0;strokeWidth=2;"
+        "verticalLabelPosition=middle;" + NODE
     )
     LARROW = "flipH=1;" + RARROW
     IMAGE = (
-        "shape=image;verticalLabelPosition=bottom;labelBackgroundColor=default;verticalAlign=top;aspect=fixed;imageAspect=0;image={image};"
-        + NODE
+        "shape=image;verticalLabelPosition=bottom;labelBackgroundColor=default;verticalAlign=top;aspect=fixed;"
+        "imageAspect=0;image={image};" + NODE
     )
 
     @staticmethod
